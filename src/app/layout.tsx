9b5adbc6f12b70bac5d233db3ff9b64e11f32ebb
sirooni <82398008+sirooni@users.noreply.github.com>
2025-05-10 14:24:09 +0900

    chore: フォントの削除

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next"
-import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
 
 export const runtime = "edge"
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-})
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-})
 
 export const metadata: Metadata = {
   title: "Portfolio",
@@ -26,11 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="jp">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   )
 }
